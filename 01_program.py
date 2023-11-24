261790719c30d2ce5d6414a414a659995559574f
Mr-Sarmad <sarmadjutt136@gmail.com>
2023-11-24 21:22:35 +0500

    Initial commit

---
+++ 01_program.py
@@ -0,0 +1,14 @@
+print("Hello World")
+a=65
+b=True
+c=5.6
+d="sarmad"
+e=a*c
+f=complex(a,b)
+print(a,b,c,d,e,f)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
